fe36c9c02070696af050a8d4c26e6accdb89419f
Ricardo Castañeda <ricardojudo@gmail.com>
2017-05-08 16:24:49 -0500

    Runnable jar

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -22,22 +22,31 @@ public class Calculator {
 
 		if (args.length < 2) {
 			System.out.println("Use: Calculator <operation> <param1> <param2>");
+			return;
 		}
 
 		String operation = args[0];
 
+		int op1 = Integer.parseInt(args[1]);
+		int op2 = 0;
+		if (args.length == 3) {
+			op2 = Integer.parseInt(args[2]);
+		}
+
+		Calculator calculator = new Calculator();
+
 		switch (operation) {
 		case "add":
-
+			System.out.println(">> " + calculator.add(op1, op2));
 			break;
 		case "sub":
-
+			System.out.println(">> " + calculator.sub(op1, op2));
 			break;
 		case "abs":
-
+			System.out.println(">> " + calculator.abs(op1));
 			break;
-
 		default:
+			System.out.println("Not supported operation: add sub abs");
 			break;
 		}
 
